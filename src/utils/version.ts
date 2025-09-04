2494937198b95cc62774c05fdcefae2ed5f170bc
KTrain5369 <me@git.ktrain5369.dev>
2025-09-04 10:09:34 +1000

    commit detection is annoying

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -2,6 +2,3 @@
 export const clientVersion: string | undefined = import.meta.env.PUBLIC_CLIENT_VERSION
 // Server version
 export const serverVersion: string | undefined = import.meta.env.PUBLIC_SERVER_VERSION
-
-// Commit detection
-export const docsNext: boolean | undefined = import.meta.env.PUBLIC_NEXT as boolean
